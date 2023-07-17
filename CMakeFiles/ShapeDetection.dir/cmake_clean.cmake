edb301b8adbf554faf9d05fb63dc1d15a9d4cc1a
CppEnjoyer69 <rafalsawczyszyn@gmail.com>
2023-07-17 19:13:51 +0200

    test

---
+++ CMakeFiles/ShapeDetection.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShapeDetection.dir/shapeDetection.cpp.o"
+  "CMakeFiles/ShapeDetection.dir/shapeDetection.cpp.o.d"
+  "ShapeDetection"
+  "ShapeDetection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShapeDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ shapeDetection.cpp
@@ -0,0 +1,16 @@
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+
+ cout << "spocona moszna 23";
+    return 0;
+}
