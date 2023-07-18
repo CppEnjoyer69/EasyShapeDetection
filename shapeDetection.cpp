b3b93164ae2335879f7b3a0e86b5555d8e5a3897
CppEnjoyer69 <rafalsawczyszyn@gmail.com>
2023-07-18 18:38:39 +0200

    added shape dect

--- shapeDetection.cpp
+++ shapeDetection.cpp
@@ -7,16 +7,38 @@
 
 using namespace cv;
 using namespace std;
-string path = "Resources/cards.jpg";
+string path = "Resources/shapes2.jpg";
 Mat img = imread(path);
 Mat imgGray, imgBlur, imgCanny, imgDil;
-
 void getContours(Mat imgDil, Mat img)
 {
   vector<vector<Point>> contours;
   vector<Vec4i> hierarchy;
   findContours(imgDil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-  drawContours(img, contours, -1, Scalar(255, 0, 255), 2);
+  vector<vector<Point>> cornPoly(contours.size());
+  vector<Rect> boundRect(contours.size());
+  string objectType;
+  for(int i = 0; i < contours.size(); ++i)
+  {
+    int area = contourArea(contours[i]);
+    if(area >= 3000)
+    {
+      float peri = arcLength(contours[i], true);
+      approxPolyDP(contours[i], cornPoly[i], 0.02*peri, true);
+      drawContours(img, cornPoly, i, Scalar(255, 0, 255), 2);
+      int objCor = static_cast<int>(cornPoly[i].size());
+      boundRect[i] = boundingRect(cornPoly[i]);
+        switch(objCor)
+        {
+          case 3 : objectType = "Triangle";
+            break;
+          case 4 : objectType = "Rectangle";
+            break;
+          default : objectType = "Circle";
+        }
+      putText(img, objectType, {boundRect[i].x, boundRect[i].y - 5}, FONT_HERSHEY_DUPLEX, 0.75, Scalar(0, 0 ,0), 2);
+    }
+}
 }
 int main()
 {
@@ -28,9 +50,6 @@ int main()
   dilate(imgCanny, imgDil, kernel);
   
   getContours(imgDil, img);
-
-
-
 
   imshow("contours", img);
 waitKey(0);
